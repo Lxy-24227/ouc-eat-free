6f1d3239176bceb918c2804f7ec63a229f214e8b
TaNuKi <1784848846@qq.com>
2026-03-11 01:16:33 +0800

    chore: 配置登录、注册及个人中心页面的路由 🔧

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,12 +2,22 @@ import { createRouter, createWebHistory } from 'vue-router'
 import CreateDish from '../pages/CreateDish.vue'
 import DishRank from '../pages/DishRank.vue'
 import DishDetail from '../pages/DishDetail.vue'
+// 👇 新增的三个页面导入
+import Login from '../pages/Login.vue'
+import Register from '../pages/Register.vue'
+import Profile from '../pages/Profile.vue'
 
 const routes = [
-  { path: '/', redirect: '/CreateDish' },
+  // 建议将根目录重定向到榜单页，这样用户一进来就能看到菜品
+  { path: '/', redirect: '/DishRank' },
   { path: '/CreateDish', name: 'CreateDish', component: CreateDish },
   { path: '/DishRank', name: 'DishRank', component: DishRank },
   { path: '/dish/:id', name: 'DishDetail', component: DishDetail },
+
+  // 👇 新增的路由配置
+  { path: '/login', name: 'Login', component: Login },
+  { path: '/register', name: 'Register', component: Register },
+  { path: '/profile', name: 'Profile', component: Profile },
 ]
 
 const router = createRouter({
